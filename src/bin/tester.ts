06dbadf5745fd514c7f71149701870315157f167
Jerome Ziegelaar <jerome.ziegelaar@gmail.com>
2019-11-27 16:25:43 +0530

    Link package scripts to dist

--- src/bin/tester.ts
+++ src/bin/tester.ts
@@ -7,35 +7,33 @@ const yargs = require('yargs');
 let path = '';
 
 const argv = yargs
-    .command('path', 'The path to look for test files in', {
-        path: {
-            description: 'the path',
-            alias: 'p',
-            type: 'string'
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+  .command('path', 'The path to look for test files in', {
+    path: {
+      description: 'the path',
+      alias: 'p',
+      type: 'string',
+    },
+  })
+  .help()
+  .alias('help', 'h').argv;
 
 if (argv.path) {
-    console.log('path is: ', argv.path);
-    path = argv.path;
-    if (!path.endsWith('/')) path += '/';
+  console.log('path is: ', argv.path);
+  path = argv.path;
+  if (!path.endsWith('/')) path += '/';
 }
 
-(async () => {  
-    const files = await test.findFiles(path);
-    if (files && files.length > 0) {
-        console.log(colors.green('Found files: ') + files);
-        files.forEach((file: string) => {
-            console.log('Loading file: '.green + file);
-            if (file) require(path + file);
-        });
-    } else {
-        console.warn('No files found'.yellow);
-    }
-})()
-.catch(err => console.error('Error: '.red, err));
+(async () => {
+  const files = await test.findFiles(path);
+  if (files && files.length > 0) {
+    console.log(colors.green('Found files: ') + files);
+    files.forEach((file: string) => {
+      console.log('Loading file: '.green + file);
+      if (file) require(path + file);
+    });
+  } else {
+    console.warn('No files found'.yellow);
+  }
+})().catch(err => console.error('Error: '.red, err));
 
 export {};
--- src/lib/test.ts
+++ src/lib/test.ts
@@ -1,13 +1,13 @@
 import globby = require('globby');
 import colors = require('colors');
- 
+
 async function findFiles(directory: string) {
-    console.log(colors.yellow("Searching for files in: ") + directory);
-    const paths = await globby(['**/*.test.js'], {
-        cwd: directory
-    });
-    
-    return paths;
-};
+  console.log(colors.yellow('Searching for files in: ') + directory);
+  const paths = await globby(['**/*.test.js'], {
+    cwd: directory,
+  });
+
+  return paths;
+}
 
 module.exports.findFiles = findFiles;
