2885369b3c102add83c9f2fdde374f136aa54168
Jerome Ziegelaar <jerome.ziegelaar@gmail.com>
2019-11-27 15:31:41 +0530

    Get path from args

--- bin/tester.js
+++ bin/tester.js
@@ -2,19 +2,38 @@
 
 const colors = require('colors');
 const test = require('../lib/test');
+const yargs = require('yargs');
 
-const path = '/Users/jerome/projects/testfiles/';
+let path = '';
+
+const argv = yargs
+    .command('path', 'The path to look for test files in', {
+        path: {
+            description: 'the path',
+            alias: 'p',
+            type: 'string'
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+if (argv.path) {
+    console.log('path is: ', argv.path);
+    path = argv.path;
+    if (!path.endsWith('/')) path += '/';
+}
 
 (async () => {  
     const files = await test.findFiles(path);
-    console.log('Found files: '.green + files);
-    if (files) {
+    if (files && files.length > 0) {
+        console.log('Found files: '.green + files);
         files.forEach(file => {
             console.log('Loading file: '.green + file);
             if (file) require(path + file);
         });
     } else {
-        console.error('No files found'.red);
+        console.warn('No files found'.yellow);
     }
 })()
 .catch(err => console.error('Error: '.red, err));
