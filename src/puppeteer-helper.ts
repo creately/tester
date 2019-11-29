760846946f1db6a39d18c85c9b11cbb17e70e2f9
Jerome Ziegelaar <jerome.ziegelaar@gmail.com>
2019-11-29 14:49:19 +0530

    Add comments for params

--- src/puppeteer-helper.ts
+++ src/puppeteer-helper.ts
@@ -2,6 +2,7 @@ import * as puppeteer from 'puppeteer';
 
 /**
  * A function that creates a puppeteer browser instance
+ * @param options an options object to be passed to the puppeteer instance
  * @returns void or a puppeteer browser instance
  */
 export async function getBrowser(options: {} = {}): Promise<puppeteer.Browser> {
