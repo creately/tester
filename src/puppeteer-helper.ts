dea0d6bf7659872a8d87f568f02d30498a7802dd
Jerome Ziegelaar <jerome.ziegelaar@gmail.com>
2019-11-29 14:52:10 +0530

    Update comment to specify correct return type

--- src/puppeteer-helper.ts
+++ src/puppeteer-helper.ts
@@ -3,7 +3,7 @@ import * as puppeteer from 'puppeteer';
 /**
  * A function that creates a puppeteer browser instance
  * @param options an options object to be passed to the puppeteer instance
- * @returns void or a puppeteer browser instance
+ * @returns a puppeteer browser instance
  */
 export async function getBrowser(options: {} = {}): Promise<puppeteer.Browser> {
   return await puppeteer.launch(options);
