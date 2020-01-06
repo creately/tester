97fec48186ad9c97e7fdffa08b1480ea6def16e7
yanushac <yanusha@cinergix.com>
2020-01-06 12:59:30 +0530

    change the x,y cordinates

--- src/actions/drag-and-drop.ts
+++ src/actions/drag-and-drop.ts
@@ -11,9 +11,13 @@ export default class DragAndDrop implements Action {
     await page.mouse.move(71, 184);
     await page.waitFor(500);
     await page.mouse.down();
-    await page.mouse.move(123, 345);
+    console.log("down");
+    //await page.mouse.move(123, 345,{steps:200});
+    await page.mouse.move(250,350);
+    console.log("moved")
     await page.mouse.up();
-    await page.waitFor(500);
+    await page.mouse.down();
+    //await page.waitFor(500);
     console.log("end*******");
     return ['data'];
   }
