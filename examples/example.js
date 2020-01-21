7008e8e2f826330a16417132590974a0c0fdc78e
Jerome Ziegelaar <jerome.ziegelaar@gmail.com>
2020-01-21 11:10:32 +0530

    Update example test

--- examples/example.js
+++ examples/example.js
@@ -7,20 +7,65 @@ var GetElementDimensions = tester.GetElementDimensions;
 var GetElementClasses = tester.GetElementClasses;
 var GetPageTitle = tester.GetPageTitle;
 var GetUrl = tester.GetUrl;
-
+var WaitForElementLocated = tester.WaitForElementLocated;
+var WaitForElementNotVisible = tester.WaitForElementNotVisible;
+var ClickElement = tester.ClickElement;
+var WaitTime = tester.WaitTime;
+var DragAndDropCoordinates = tester.DragAndDropCoordinates;
+var DragAndDropElement = tester.DragAndDropElement;
 
 var Equals = tester.Equals;
 var GreaterThan = tester.GreaterThan;
 var LessThan = tester.LessThan;
 var Includes = tester.Includes;
 
-tester.registerActions(GoTo, ResizeViewport, GetPageDimensions, GetViewportDimensions, GetElementDimensions, GetElementClasses, GetPageTitle, GetUrl);
+tester.registerActions(GoTo, ResizeViewport, GetPageDimensions, GetViewportDimensions, GetElementDimensions, GetElementClasses, GetPageTitle, GetUrl, WaitForElementLocated, WaitForElementNotVisible, ClickElement, WaitTime, DragAndDropCoordinates, DragAndDropElement);
 tester.registerAsserts(Equals, GreaterThan, LessThan, Includes);
 
 tester.addTest('simple test', [
     {
         title: 'goes to localhost',
         action: GoTo,
-        args: ['http://localhost:4200']
+        args: ['https://creately.com/demo-start']
+    },
+    {
+        title: 'waits for loader overlay',
+        action: WaitForElementLocated,
+        args: ['/html/body/app-root/main-loader']
+    },
+    {
+        title: 'waits for loader overlay to dissappear',
+        action: WaitForElementNotVisible,
+        args: ['/html/body/app-root/main-loader']
+    },
+    {
+        title: 'click blank document button',
+        action: ClickElement,
+        args: ['/html/body/app-root/ng-component/div/div/div[2]/perfect-scrollbar/div/div[1]/div[1]/div/div[1]/creator-thumbnail/div/div[1]']
+    },
+    {
+        title: 'waits for maybe later button',
+        action: WaitForElementLocated,
+        args: ['//button[contains(text(),"Maybe Later")]']
+    },
+    {
+        title: 'clicks maybe later button',
+        action: ClickElement,
+        args: ['//button[contains(text(),"Maybe Later")]']
+    },
+    {
+        title: 'waits for first shape in library',
+        action: WaitForElementLocated,
+        args: ['/html/body/app-root/ng-component/div/div/div[2]/left-sidebar/div/div[2]/left-bar/div/div/library-container/div/div[3]/perfect-scrollbar/div/div[1]/collapsible-menu/div/collapsible-menu-item[1]/div/div[2]/ng-component/library-tile[1]/div']
+    },
+    {
+        title: 'drag and drop first shape in library to the canvas',
+        action: DragAndDropElement,
+        args: ['/html/body/app-root/ng-component/div/div/div[2]/left-sidebar/div/div[2]/left-bar/div/div/library-container/div/div[3]/perfect-scrollbar/div/div[1]/collapsible-menu/div/collapsible-menu-item[1]/div/div[2]/ng-component/library-tile[1]/div', 300, 300]
+    },
+    {
+        title: 'move shape in center of canvas',
+        action: DragAndDropCoordinates,
+        args: [820, 370, 950, 380, 3000]
     },
 ]);
