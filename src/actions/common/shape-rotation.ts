3ff711cf357a12c0def2e3e05455f69829f68754
yanushac <yanusha@cinergix.com>
2020-02-05 10:51:11 +0530

    add shape rotation action

---
+++ src/actions/common/shape-rotation.ts
@@ -0,0 +1,18 @@
+import Action from "../../action.i";
+import { WebDriver } from "selenium-webdriver";
+
+export default class ShapeRotation implements Action {
+    async execute(args: any[], context: any): Promise<string[]> {
+        var driver: WebDriver = context.driver;
+
+        await driver
+            .actions({ bridge: true })
+            .pause(4000)
+            .move({ x: args[0], y: args[1] })
+            .press()
+            .move({ x: args[2], y: args[3] })
+            .pause(3000)
+            .perform();
+        return [''];
+    }
+}
