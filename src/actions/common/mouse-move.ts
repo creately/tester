589ca36cc85d7d718409d0a4b4f7aa8de669bc69
yanushac <yanusha@cinergix.com>
2020-02-05 11:53:34 +0530

    add npm package to get cursor position

--- src/actions/common/mouse-move.ts
+++ src/actions/common/mouse-move.ts
@@ -1,10 +1,11 @@
 import Action from '../../action.i';
 import { WebDriver } from 'selenium-webdriver';
+import GetCursorPosition from 'cursor-position'
 
 export default class MouseMove implements Action {
   async execute(args: any[], context: any): Promise<string[]> {
     var driver: WebDriver = context.driver;
-    
+  
     await driver
       .actions({ bridge: true })
       .pause(4000)
@@ -13,6 +14,11 @@ export default class MouseMove implements Action {
       .release()
       .pause(3000)
       .perform();
+      document.addEventListener('mousemove', () => {
+        const {x, y} = GetCursorPosition()
+        console.log(x, y)
+      })
+      
     return [''];
   }
 }
