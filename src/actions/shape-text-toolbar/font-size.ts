7e05060633650bc154765da9690b56c807c4e95b
yanushac <yanusha@cinergix.com>
2020-02-12 12:00:10 +0530

    resoled the confilct of font size action

--- src/actions/shape-text-toolbar/font-size.ts
+++ src/actions/shape-text-toolbar/font-size.ts
@@ -5,7 +5,6 @@ import Action from '../../action.i';
      * Change the font size of text
 */
 export default class FontSize implements Action {
-<<<<<<< HEAD
     async execute(args: any[], context: any): Promise<string[]> {
         var driver: WebDriver = context.driver;
         const fontSizeBtn = await driver.findElement(By.xpath('//*[@id="shape-text-toolbar"]/div/toolbar-input-list-dropdown'));
@@ -16,28 +15,5 @@ export default class FontSize implements Action {
             await fontSize.click();
         }
         return [args.toString()];
-=======
-  async execute(args: any[], context: any): Promise<string[]> {
-    var driver: WebDriver = context.driver;
-    const fontSizeBtn = await driver.findElement(
-      By.xpath('//*[@id="shape-text-toolbar"]/div/toolbar-input-list-dropdown')
-    );
-    await fontSizeBtn.click();
-    const dropDownList = await driver.findElement(
-      By.xpath('//*[@id="shape-text-toolbar"]/div/toolbar-input-list-dropdown/div/abs-dropdown/div/div[2]/div/ul')
-    );
-    if (await dropDownList.isDisplayed()) {
-      const fontSize = await driver.findElement(
-        By.xpath(
-          '//*[@id="shape-text-toolbar"]/div/toolbar-input-list-dropdown/div/abs-dropdown/div/div[2]/div/ul/perfect-scrollbar/div/div[1]/toolbar-list-dropdown-item[' +
-            args[0] +
-            ']'
-        )
-      );
-      await fontSize.click();
->>>>>>> 028d4da49d17459db163a5c90538521a44ba0dc1
     }
-
-    return [args.toString()];
-  }
 }
